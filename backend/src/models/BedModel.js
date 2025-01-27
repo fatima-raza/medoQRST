520e8170581e209a137bce558c1273afa2dc8fc7
Fatima Raza <fraza30512@gmail.com>
2025-01-27 14:49:19 +0500

    Removed the old dashboard folder

---
+++ backend/src/models/BedModel.js
@@ -0,0 +1,9 @@
+class Bed{
+    constructor(Bed_no, Ward_no, QRcode){
+        this.Bed_no = Bed_no;
+        this.Ward_no = Ward_no;
+        this.QRcode = QRcode;
+    }
+}
+
+module.exports = Bed;
