85a54ebd73d9182852b9f83cae50826ee0b1d09e
Fatima Raza <fraza30512@gmail.com>
2025-01-28 00:30:05 +0500

    Removed unwanted folders and updated .gitignore

--- backend/src/models/BedModel.js
+++
@@ -1,9 +0,0 @@
-class Bed{
-    constructor(Bed_no, Ward_no, QRcode){
-        this.Bed_no = Bed_no;
-        this.Ward_no = Ward_no;
-        this.QRcode = QRcode;
-    }
-}
-
-module.exports = Bed;
--- backend/src/routes/BedRoutes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const bedController = require('../controllers/BedController');
-const router = express.Router();
-
-router.get('/bed', bedController.getBeds);
-router.get('/bed/:bedNo/:wardNo', bedController.getBed);
-
-module.exports = router;
